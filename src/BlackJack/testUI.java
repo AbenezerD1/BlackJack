a69fc62681bb8cedb4aca9a9fb40870aa81d7243
Abenezer <aben3@uw.edu>
2024-05-27 16:11:57 -0700

    add more UI test for drawing dealers hand and main deck

--- src/BlackJack/testUI.java
+++ src/BlackJack/testUI.java
@@ -30,27 +30,35 @@ public class testUI {
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            Player jhon = new Player(1,new Deck(),1000);
-            Player jhon2 = new Player(2,new Deck(),1000);
+            Player[] players = {
+                    new Player(1,new Deck(),1000),
+                    new Player(2,new Deck(),1000),
+            };
+            Dealer dealer = new Dealer();
             Deck deck = new Deck();
-            Card card1= new Card(CardValues.SIX, Suit.CLUB, 6);
-            Card card2 = new Card(CardValues.SEVEN, Suit.HEART, 7);
-            Card card3 = new Card(CardValues.FIVE, Suit.SPADE, 5);
-            Card card4 = new Card(CardValues.TEN, Suit.DIAMOND, 10);
-            Card card5 = new Card(CardValues.ACE, Suit.SPADE, 10);
-            card1.flip();
-            jhon.hit(card1);
-            jhon.hit(card2);
-            jhon.hit(card3);
-            jhon.hit(card4);
-            jhon.hit(card5);
-            jhon2.hit(card1);
-            jhon2.hit(card2);
-            jhon2.hit(card3);
-            jhon2.hit(card4);
-            jhon2.hit(card5);
-            jhon.drawPlayerHand(g,10,350,15,0.15);
-            jhon2.drawPlayerHand(g,535,350,15,0.15);
+            Card[] c = {
+                    new Card(CardValues.SIX, Suit.CLUB, 6),
+                    new Card(CardValues.SEVEN, Suit.HEART, 7),
+                    new Card(CardValues.FIVE, Suit.SPADE, 5),
+                    new Card(CardValues.TEN, Suit.DIAMOND, 10),
+                    new Card(CardValues.ACE, Suit.SPADE, 10),
+            };
+
+            for(int j = 0; j < players.length; j++){
+                for(int i = 0; i < c.length; i++){
+                    players[j].hit(c[i]);
+                }
+            }
+
+            c[0].flip();
+            dealer.addCardToDealersDeck(c[0]);
+            dealer.addCardToDealersDeck(c[1]);
+            dealer.flipMainDeck();
+
+            players[0].drawPlayerHand(g,10,350,15,0.15);
+            players[1].drawPlayerHand(g,535,350,15,0.15);
+            dealer.drawDealerHand(g,100,50,0,0.25);
+            dealer.drawMainDeck(g,750,50,0.19);
         }
     }
 }
