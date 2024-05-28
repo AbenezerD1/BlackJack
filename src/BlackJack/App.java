302ccd3626680441f678feb8bc5650d576a3a1db
Abenezer <aben3@uw.edu>
2024-05-28 16:30:41 -0700

    added app java

---
+++ src/BlackJack/App.java
@@ -0,0 +1,10 @@
+package BlackJack;
+
+/**
+ * {@code @Version} 0.0.0
+ */
+public class App {
+    public static void main (String[] args) throws Exception{
+        BlackJack blackJack = new BlackJack();
+    }
+}
